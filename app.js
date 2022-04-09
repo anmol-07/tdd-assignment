a33cb0b39ca44bebe3eef86a0ee80706691a21a8
anmol-07 <anmolseth874@gmail.com>
2022-04-09 14:27:06 +0530

    added test case to handle unknown amount of numbers

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-function sumOfNumbersPassedInString(commaSeperatedStringOfNumbers) {
+function add(commaSeperatedStringOfNumbers) {
   const numberArray = commaSeperatedStringOfNumbers.split(",");
   let totalSum = 0;
   for (let i = 0; i < numberArray.length; i++) {
-    console.log(numberArray[i]);
     totalSum += Number(numberArray[i]);
   }
   return totalSum;
 }
-module.exports = sumOfNumbersPassedInString;
+module.exports = add;
--- app.test.js
+++ app.test.js
@@ -1,13 +1,32 @@
-const sumOfNumbersPassedInString = require("./app");
+const add = require("./app");
 
 test("First test case for empty string and expecting a 0", () => {
-  expect(sumOfNumbersPassedInString("")).toBe(0);
+  expect(add("")).toBe(0);
 });
 
 test("Second test case for passing only a single number i.e. 1 in string", () => {
-  expect(sumOfNumbersPassedInString("1")).toBe(1);
+  expect(add("1")).toBe(1);
 });
 
 test("Third test case for passing two number i.e. 1 and 2 in string", () => {
-  expect(sumOfNumbersPassedInString("1,2")).toBe(3);
+  expect(add("1,2")).toBe(3);
+});
+
+test("Test case to handle unknown amount of numbers", () => {
+  // creating a function to get array with unknown amount of numbers
+  function createRandomArray(length, max) {
+    return [...new Array(length)].map(() => Math.round(Math.random() * max));
+  }
+
+  let randomInteger = Math.floor(Math.random() * 100 + 1); //getting some random numbers
+
+  let resultingRandomArray = createRandomArray(randomInteger, randomInteger);
+
+  // sums up the resultant array
+  const sumOfArrayOfUnknownAmountOfNumbers = resultingRandomArray.reduce((previousValue, nextValue) => previousValue + nextValue, 0);
+
+  // converts the array to a string
+  let resultingRandomArrayToString = resultingRandomArray.join();
+
+  expect(add(resultingRandomArrayToString)).toBe(sumOfArrayOfUnknownAmountOfNumbers);
 });
